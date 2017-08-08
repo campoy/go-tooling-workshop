6d501b1c1a339c5c0c1ea71661fd0e1ccd3d74f2
Marwan Sulaiman <marwan.sulaiman@work.co>
2017-08-08 17:48:42 -0400

    rm localhost to make running examples in Docker accessible (#36)

--- 3-dynamic-analysis/webserver/main.go
+++ 3-dynamic-analysis/webserver/main.go
@@ -23,7 +23,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
